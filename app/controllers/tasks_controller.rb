4a4b95f40711520117d30fbc96d4cea9220f5874
zita kerepesi <kerepesi.zita@me.com>
2020-05-12 16:57:14 +0100

    rails task manager completed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+ private
+
+  def task_params
+    params.require(:task).permit(:title, :completed, :details)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmlend
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "task/new", to: "tasks#new"
+  get "task/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
